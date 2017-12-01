473520295d865dd5a382a34ab169a7712b1fadbd
dimaspirit <dspwww@gmail.com>
2017-12-01 14:44:05 +0200

    working

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -70,10 +70,19 @@ class Client extends User {
    *  - Nothing.
    * At this way you auth by application session with READ only rights;
    * 
+   * - Quickblox Session token.
+   * You pass token and SDK gives from Quickblox server all information
+   * about session and user if this User session token. 
+   * If is Application session token user will be null.
+   * 
    *  - A login & a password or an email & the password.
    * At this way you auth as user with User session and CRUD rights;
    * 
-   * @param {object} [creds] You pass 
+   * @param {object} [creds] Data to authorize in Quickblox API Server
+   * @param {string} [creds[].token] Quicblox Session Token (Application or User)
+   * @param {string} [creds[].login] Login of existed user (uses ONLY with password property)
+   * @param {string} [creds[].email] Email of existed user (uses ONLY with password property)
+   * @param {string} [creds[].password] Password of existed user (uses with login or email property)
    * 
    * @example 
    * import Quickblox from 'quickblox-javascript-sdk';
@@ -87,6 +96,13 @@ class Client extends User {
    * // or 
    * // const creds = {'token': 'JydmzsuUjhnzx8asj'};
    * 
+   * // or 
+   * // const userCreds = {
+   * //   email: 'helloworld@net.com',
+   * //   password: 'secretword'
+   * // }
+   * // const clientInstance = new QB(userCreds);
+   * 
    * const clientInstance = new QB(creds);
    * 
    * clientInstance.auth().then( (token) => {
@@ -99,7 +115,18 @@ class Client extends User {
     const self = this;
 
     return new Promise((resolve, reject) => {
-      if(!userCreds) {
+      if(creds && creds.token) {
+        this._syncSession(creds.token).then( (responce) => {
+          let data = responce.data.session;
+
+          self.session_token = data.token;
+          self.user_id = data.user_id !== 0 ? data.user_id : null;
+
+          // TODO: if user_id !== 0 -> init User info
+
+          resolve(self.session_token);
+        });
+      } else if(!creds) {
         this._createApplicationSession().then((responce) => {
           let data = responce.data.session;
           
@@ -110,7 +137,7 @@ class Client extends User {
         }).catch(error => {
           reject(error);
         });
-      }  
+      }
     });
   }
   _createApplicationSession() {
@@ -132,36 +159,22 @@ class Client extends User {
     });
   }
 
-  // _authByExistSession(sessionToken) {
-  //   const self = this;
+  _syncSession(token) {
+    const self = this;
+    
+    return new Promise((resolve, reject) => {
 
-  //   // return new Promise 
-
-  //   this.service({
-  //     method: 'GET',
-  //     url: `${self._urls.session}.json`,
-  //     headers: {
-  //       'QB-Token': sessionToken
-  //     }
-  //   }).then( (responce) => {
-  //     const userId = responce.data.session.user_id;
-
-  //     // if(userId && userId !== 0) {
-        
-  //     // } 
-
-  //     // Get user info / if session with user id
-
-  //     console.log('AA');
-  //     console.log(responce.data.session.user_id);
-
-  //     // resolve(data);
-  //   }).catch(function(error) {
-  //     console.log('BB');
-  //     console.log(error);
-  //     // reject(error);
-  //   });
-  // }
+      this.service({
+        method: 'GET',
+        url: `${self._urls.session}.json`,
+        headers: {'QB-Token': token}
+      }).then(function(data) {
+        resolve(data);
+      }).catch(function(error) {
+        reject(error);
+      });
+    });
+  }
 
   _generateAuthMessage(appId, authKey) {
     /**
